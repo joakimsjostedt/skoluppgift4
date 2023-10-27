1a23da2b0e3aa38ab4942f8284eda94eaee5292f
Joakim Sjöstedt <joakim.sjostedt@softhouse.se>
2023-10-27 23:01:55 +0200

    hej

---
+++ skoluppgift4/skoluppgift4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  skoluppgift4
+//
+//  Created by Joakim Sjöstedt on 2023-10-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ skoluppgift4/skoluppgift4/skoluppgift4App.swift
@@ -0,0 +1,17 @@
+//
+//  skoluppgift4App.swift
+//  skoluppgift4
+//
+//  Created by Joakim Sjöstedt on 2023-10-27.
+//
+
+import SwiftUI
+
+@main
+struct skoluppgift4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
